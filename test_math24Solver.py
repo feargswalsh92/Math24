e145b5313101b177706233dab7ce4ad3b4fd0ccc
Feargal Walsh <feargswalsh@gmail.com>
2017-04-26 18:32:32 -0500

    Tests added

--- test_math24Solver.py
+++ test_math24Solver.py
@@ -5,6 +5,11 @@ from Math24 import Math24Solver
 
 class TestMath24Solver(TestCase):
     def test__calculateEquation(self):
+        #self.fail()
         self.assertEqual(Math24Solver._calculateEquation(self,2,"+",3),5)
+     #    self.assertEqual(Math24Solver._calculateEquation(self, 2, "-", 3), -1)
+     #   #'  self.assertEqual(Math24Solver._calculateEquation(self, 2, "-", 3), -1)
+     # #   self.assertEqual(Math24Solver._calculateEquation(self, 2, "-", 3), -1)
+
 
 
